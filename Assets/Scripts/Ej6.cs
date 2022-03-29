202c54027c1c83d4d9082016edb3dcc9e96af298
MatiasBlanco0 <101400282+MatiasBlanco0@users.noreply.github.com>
2022-03-29 14:10:52 -0300

    Programacion del script Ej6

--- Assets/Scripts/Ej6.cs
+++ Assets/Scripts/Ej6.cs
@@ -4,10 +4,30 @@ using UnityEngine;
 
 public class Ej6 : MonoBehaviour
 {
+    /*Realizá un programa que permita el ingreso por Inspector de un valor entero mayor que 0
+    en una variable llamada num1 y muestre un mensaje por pantalla indicando "el número es
+    par" o "el número es impar". Deberá utilizar el operador “módulo” es el caracter %.*/
+    public int num1 = 0;
+    int resultado;
     // Start is called before the first frame update
     void Start()
     {
-        
+        if(num1 != 0)
+        {
+            resultado = num1 % 2;
+            if(resultado == 0)
+            {
+                Debug.Log("el numero es par");
+            }
+            else
+            {
+                Debug.Log("el numero es impar");
+            }
+        }
+        else
+        {
+            Debug.Log("El numero era menor o igual a 0");
+        }
     }
 
     // Update is called once per frame
