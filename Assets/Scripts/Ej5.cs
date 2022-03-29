fef49de889cddebca9eb2b85f0fc379bdc5e62d5
MatiasBlanco0 <101400282+MatiasBlanco0@users.noreply.github.com>
2022-03-29 14:00:58 -0300

    Programacion de Ej5

--- Assets/Scripts/Ej5.cs
+++ Assets/Scripts/Ej5.cs
@@ -4,10 +4,24 @@ using UnityEngine;
 
 public class Ej5 : MonoBehaviour
 {
+    /*Realizá un programa que, dados dos números enteros num1 y num2 cargados desde el
+    Inspector muestre el resultado de la división del primero por el segundo en la consola
+    siguiendo el formato num1 + “ / “ + num2 + “ = “ + resultado.
+    En el caso de que el segundo valor num2 sea cero, mostrar el siguiente mensaje de error:
+    “No se puede dividir por cero”.*/
+    public int num1;
+    public int num2;
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (num2 != 0)
+        {
+            Debug.Log(num1 + " / " + num2 + " = " + (num1 / num2));
+        }
+        else
+        {
+            Debug.Log("No se puede dividir por cero");
+        }
     }
 
     // Update is called once per frame
