fc456392dd6bb85c8680ccec49406e1722de5387
MatiasBlanco0 <101400282+MatiasBlanco0@users.noreply.github.com>
2022-03-29 13:38:03 -0300

    Creacion de Escena 2 y programcion de Ejercicio 2

--- Assets/Scripts/Ej1.cs
+++ Assets/Scripts/Ej1.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class Ej1 : MonoBehaviour
 {
+    /*Realizá un programa donde se cree una variable entera llamada num1, que inicialmente
+    valdrá 0. Luego incrementá su valor en 2 y mostralo por pantalla.Después mostrá el
+    resultado de multiplicarlo por sí mismo.*/
+
     int num1 = 0;
     // Start is called before the first frame update
     void Start()
